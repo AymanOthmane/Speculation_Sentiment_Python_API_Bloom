240c1912ff0089d01840e675d9a7f8f1e0cbfd84
Ayman Othmane <148972573+AymanOthmane@users.noreply.github.com>
2024-08-26 16:06:51 +0200

    Main and bat file

---
+++ libraries.bat
@@ -0,0 +1,17 @@
+@echo off
+REM Install Python libraries
+
+pip install numpy
+pip install pandas
+pip install matplotlib
+pip install statsmodels
+pip install scikit-learn
+pip install blpapi
+pip install yfinance
+pip install streamlit
+pip install pykalman
+pip install tensorflow
+pip install ta
+
+echo All libraries installed successfully.
+pause
